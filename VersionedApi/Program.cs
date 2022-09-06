7efdae43c987f963c7271ea96745de9b398d410e
Kamil Preweda <preweda.kamil@o2.pl>
2022-09-06 19:14:04 +0200

    Configured Swagger to better demostrate API versioning

--- VersionedApi/Program.cs
+++ VersionedApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,12 +7,50 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opts =>
+{
+    var title = "Our Versioned API";
+    var description = "This is a Web API that demonstrated versioning.";
+    var terms = new Uri("https://localhost:7214/terms");
+    var license = new OpenApiLicense()
+    {
+        Name = "This is my full license information or a link to it."
+    };
+    var contact = new OpenApiContact()
+    {
+        Name = "Tim Corey Helpdesk",
+        Email = "help@iamtimcorey.com",
+        Url = new Uri("https://www.IAmTimCorey.com")
+    };
+    opts.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = $"{title} v1",
+        Description = description,
+        TermsOfService = terms,
+        License = license,
+        Contact = contact
+    });
+    opts.SwaggerDoc("v2", new OpenApiInfo
+    {
+        Version = "v2",
+        Title = $"{title} v2",
+        Description = description,
+        TermsOfService = terms,
+        License = license,
+        Contact = contact
+    });
+});
 builder.Services.AddApiVersioning(opts =>
 {
     opts.AssumeDefaultVersionWhenUnspecified = true;
     opts.DefaultApiVersion = new(1, 0);
     opts.ReportApiVersions = true;
+});
+builder.Services.AddVersionedApiExplorer(opts =>
+{
+    opts.GroupNameFormat = "'v'VVV";
+    opts.SubstituteApiVersionInUrl = true;
 });
 
 var app = builder.Build();
@@ -19,7 +59,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(opts =>
+    {
+        opts.SwaggerEndpoint("/swagger/v2/swagger.json", "v2");
+        opts.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    });
 }
 
 app.UseHttpsRedirection();
